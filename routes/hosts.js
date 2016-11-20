486cb17f6dfa5d352950857e2ec76c60849eb472
kushal dileep joshi <kushal dileep joshi>
2016-11-20 03:22:24 -0800

    testproject

--- routes/hosts.js
+++ routes/hosts.js
@@ -7,64 +7,199 @@ var mongoURL = "mongodb://apps92:shim123@ds155727.mlab.com:55727/airbnbproto";
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Hosts = require('../Models/host');
+var bodyParser = require('body-parser').json();
+var uniqueIDGenerator = require('../routes/uniqueIDGenerator');
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
+
+
+passport.use('host',new LocalStrategy({
+			    usernameField: 'email'
+			},
+		  function(username, password, done) {
+			  console.log("I am checking here"+username);
+			  console.log("I am checking password"+password);
+			  Hosts.findOne({ email: username }, function (err, user) {
+		      if (err) { return done(err); }
+		      if (!user) {
+		    	  console.log("Wrong Email");
+		        return done(null, false, { message: 'Incorrect email.' });
+		      }		      
+		      var hash = user.password;
+				console.log(hash);
+		        if(!bcrypt.compareSync(password, hash)){
+		        	console.log("Wrong password");
+		        	return done(null, false, { message: 'Incorrect password.' });
+		        }
+		        console.log("Correct password");
+		      
+		      return done(null, user);
+		    });
+		  }
+));
+
 
 var HostSignUp = function(req,res){
 	console.log("Inside signup host");
-	/*var newHost = Hosts;
-	newHost.host_id = "123456";
-	newHost.firstname = "yashas";
-	newHost.lastname = "malleshappa";
-	newHost.email = "yashasm1991@gmail.com";
-
-	console.log(newHost);*/
+	console.log(req.body.hostSignUp);
+	console.log("Testing input");
+	console.log(req.body.hostSignUp.email);
+	console.log("Testing output");
+	
+	Hosts.findOne({"email":req.body.hostSignUp.email},function(err,host){
+		console.log("Inside find");
+		if(err){
+			console.log("inside find one error");
+			res
+			.status(422)
+			.send({"result":"Invalid details"});
+			return;
+		}
+		console.log(host);
+		if(host != null){
+			console.log("inside found one existing record");
+			res
+			.status(422)
+			.send({"result":"Invalid details"});
+			return;
+		}
+		req.body.hostSignUp.host_id = uniqueIDGenerator.returnUniqueID();
+		req.body.hostSignUp.type = 2;
+		var salt = bcrypt.genSaltSync(10);
+		var hash = bcrypt.hashSync(req.body.hostSignUp.password, salt);
+		req.body.hostSignUp.password = hash;
 		
+		var newHost = new Hosts(req.body.hostSignUp);
+		newHost.save(function(err,result){
+			console.log("Inside saving record");
+			if(!err){
+				console.log("Inside saved record ");
+				console.log(result);
+				
+				res
+				.status(200)
+				.send({"result":"Host Registered"});
+				return;
+			}
+			else
+				console.log("Inside saving error ");
+				console.log(err);
+				res
+				.status(422)
+				.send({"result":"Invalid details"});
+				return;
+		});
+		
+	});
+	
 
-	res
-	.status(200)
-	.send({"result":"Host Registered"});
+	
 };
-
+/*
 var HostLogIn = function(req,res){
 	console.log("Inside Host sign in");
 	
+	Hosts.findOne({"email":req.body.HostLogIn.email},function(err,host){
+		if(err || host == null){
+			console.log("user not found");
+			res
+			.status(404)
+			.send({"result":"Host not found"});
+			return;
+		}
+				
 
-	res
-	.status(200)
-	.send({"result":"Host Logged in"});
+        var hash = host.password;
+		console.log(hash);
+        if(bcrypt.compareSync(req.body.HostLogIn.password, hash)){
+        	console.log("successful login");
+        	req.session.email = host.email;
+        	req.session.username = host.firstname;
+
+    		res
+    		.status(200)
+    		.send({"result":"Host Logged in"});
+    		return;
+        }
+
+		res
+		.status(404)
+		.send({"result":"Host Not found"});
+	})
+	
+	
 	
 };
-
+*/
 var DeleteHost = function(req,res){
 	console.log("Inside Host Delete");
 	
+	Hosts.find({ "email":req.body.DeleteHost.email }).remove( function(err,removed){		
+		console.log(removed);
+		if(err || removed == null){
+			res
+			.status(400)
+			.send({"result":"Best Request"});
+			return;
+		}
+		
+		res
+		.status(200)
+		.send({"result":"Host Deleted"});
+	} );
 
-	res
-	.status(200)
-	.send({"result":"Host Deleted"});
 };
 
 
 var UpdateHost = function(req,res){
 	console.log("Inside Host Update");
 	
+	req.body.UpdateHost.avgrating = 0;
+	
+	var query = {'email':req.body.UpdateHost.email};
+	
+	Hosts.findOneAndUpdate(query, req.body.UpdateHost, {upsert:false}, function(err, doc){
+		
+	    if (err) {
+	    	res
+			.status(400)
+			.send({"result":"Bad request"});
+			return;
+	    }
+	    
 
-	res
-	.status(200)
-	.send({"result":"Host Updated"});
+		res
+		.status(200)
+		.send({"result":"Host Updated"});
+	});
+	
+
+	
 };
 
 var GetHost = function(req,res){
 	console.log("Inside Get Host");
 	
-
-	res
-	.status(200)
-	.send({"result":"Host details"});
+	
+	Hosts.findOne({"email":req.query.email},function(err,host){
+		if(err || host == null){
+			res
+			.status(404)
+			.send({"result":"Host not found"});
+			return;
+			
+		}
+		res
+		.status(200)
+		.send({"result":host});
+	});
 	
 }; 
 
 exports.HostSignUp = HostSignUp;
-exports.HostLogIn = HostLogIn;
 exports.DeleteHost = DeleteHost;
 exports.UpdateHost = UpdateHost;
 exports.GetHost = GetHost;
