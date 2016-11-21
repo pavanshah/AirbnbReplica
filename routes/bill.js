8223719339bb5649c3e6d992fd949d09ba1b275b
Pavan Shah <pavanshah77@gmail.com>
2016-11-21 04:54:59 -0800

    Soft deletion of bills

--- routes/bill.js
+++ routes/bill.js
@@ -69,6 +69,26 @@ var SearchBillsbyDate = function (req,res){
 
 }
 
+
+var DeleteBill = function (req,res){
+
+	console.log("inside delete bill");
+	
+	bill.update({"billing_id":req.body.DeleteBill.billing_id}, {$set : {bill_status : "inactive" }}, function(err, user){
+		if (user) 
+		{
+			res.status(200);
+			res.json({"result":"Bill Deleted"});
+		} else 
+		{
+			console.log(err);
+		}
+	});
+}
+
+
+
+exports.DeleteBill = DeleteBill;
 exports.SearchBillsByMonth = SearchBillsByMonth;
 exports.GenerateBill = GenerateBill;
 exports.SearchBillsbyDate = SearchBillsbyDate;
