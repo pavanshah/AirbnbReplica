9231b76c8739aec23b7f174acc34d510952d3e9a
Pavan Shah <pavanshah77@gmail.com>
2016-11-21 00:41:19 -0800

    With client side validation for login page

--- public/scripts/loginModal.controller.js
+++ public/scripts/loginModal.controller.js
@@ -3,7 +3,9 @@ var app = angular.module("Airbnb");
 function loginModalControllerFn($uibModalInstance) {
 	var vm = this;
 	vm.userData = {};
+	vm.required = false;
 	vm.ok = function () {
+		vm.required = true;
     	$uibModalInstance.close(vm.userData);
   	};
 
