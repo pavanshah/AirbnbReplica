3b1ba67306d2347a4293e096054d583b0e1681b3
Parvez <parvez.patel@sjsu.edu>
2016-11-22 01:34:51 -0800

    property details page added, property details service called

--- public/scripts/propertyDetails.controller.js
+++ public/scripts/propertyDetails.controller.js
@@ -1,8 +1,21 @@
 var app = angular.module('Airbnb');
 
-function propertyDetailsControllerFn($state) {
+function propertyDetailsControllerFn($state,$stateParams,$http) {
 	
 	var vm = this;
+	vm.property = {};
+	function getPropertyDetails(propertyId) {
+		$http.post("/SearchPropertyById",propertyId).
+		then(function(response) {
+			if(response.status==200){
+				vm.property= response.data;
+				console.log("property",vm.property);
+			}
+		})
+	}
+
+	getPropertyDetails($stateParams.property_id);
+	console.log($stateParams);
 }
 
 app.controller('PropertyDetailsController',propertyDetailsControllerFn);
