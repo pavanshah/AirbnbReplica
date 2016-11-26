fc49d07db79baa80a47fac847fcdcb6ef82cd102
kushal dileep joshi <kushal dileep joshi>
2016-11-26 04:15:45 -0800

    hostPhotoAndVideoChanges

---
+++ public/scripts/hostProfilePhotoAndVideo.Controller.js
@@ -0,0 +1,11 @@
+var app = angular.module('Airbnb');
+
+
+function hostProfilePhotoAndVideoFn($state,$scope,$http) {
+	var vm = this;
+	
+	
+		
+}
+
+app.controller('hostProfilePhotoAndVideo',hostProfilePhotoAndVideoFn);
