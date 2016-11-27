f59aa11425462143f6f680e1d75f3bfc76c47a47
Apoorv Mehta <mehta.apoorv@hotmail.com>
2016-11-26 19:42:18 -0800

    Removed double console logging due to Morgan logger

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.set('view engine', 'ejs');
 /*app.use(express.favicon());*/
 app.use(logger('dev'));
 /*app.use(express.json());*/
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride());
