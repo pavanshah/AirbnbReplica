760b8202ea6932393bbd5dd69ef9212992ec737e
Pavan Shah <pavanshah77@gmail.com>
2016-12-05 01:52:16 -0800

    add property validation improved

--- Airbnb-RabbitMQ-BackEndServer/server.js
+++ Airbnb-RabbitMQ-BackEndServer/server.js
@@ -14,7 +14,7 @@ var review = require('./services/review');
 
 
 
-var mongoSessionConnectURL = "mongodb://apps92:shim123@ds155727.mlab.com:55727/airbnbproto";
+var mongoSessionConnectURL = "mongodb://apps92:shim123@ds113668.mlab.com:13668/airbnbprod";
 var expressSession = require("express-session");
 var mongoStore = require("connect-mongo")(expressSession);
 var mongo = require("./services/mongo");
