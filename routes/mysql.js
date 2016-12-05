7edccafbb0af5e01bdbc8a6d17d3a5435a545c6e
Vansh Zaveri <vanshzaveri93@gmail.com>
2016-12-05 01:56:51 -0800

    Bug Fix

--- routes/mysql.js
+++ routes/mysql.js
@@ -3,9 +3,9 @@
  */
 var mysql_pool = require('mysql');
 var pool  = mysql_pool.createPool({
-	host     : 'localhost',
-	user     : 'root',
-	password : 'cheese',
+	host     : 'db4free.net',
+	user     : 'airbnb',
+	password : 'airbnb',
 	port     : 3306,
 	database : 'airbnb',
 	connectionLimit : '100',
